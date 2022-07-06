90dbd64de7ed27c3c5901da4eeb63aaa02aa4229
yuta.mizusawa <yuta.mizusawa@PC211NPQ6LT.local>
2022-07-06 20:00:05 +0900

    途中

--- components/TweetCard.vue
+++ components/TweetCard.vue
@@ -1,32 +1,40 @@
 <template>
-    <div v-if="isPublic(thisTweet)">
-        <p>user:{{thisTweet.user_id}}</p>
-        <p>tweet no.{{thisTweet.id}}</p>
-        <a @click="toggleLike(thisTweet)">{{thisTweet.like_count}} likes     </a>
-        <a v-if="!liked">☆</a>
-        <a v-if="liked">★</a>
-        <p v-if="!editable" @click="toggleEdit()">{{thisTweet.text}}</p>
+    <div>
+        <h1 v-if="!editable && isMine" @click="toggleEdit()">{{thisTweet.text}}</h1>
+        <h1 v-if="!editable && !isMine">{{thisTweet.text}}</h1>
 
-        <form @submit.prevent="updateText(thisTweet)" v-if="editable" >
-            <input :placeholder="thisTweet.text" v-model="text" />
+        <form v-if="editable" @submit.prevent="updateText(thisTweet)">
+            <input v-model="text" />
             <button type="submit">submit</button>
         </form>
-        
-        <button @click="deleteTweet(thisTweet)">delete</button>
+
+        <p>user:{{thisTweet.user_id}}</p>
+
+        <p>{{thisTweet.like_count}} likes     </p>
+        <div v-if="!isMine" @click="toggleLike(thisTweet)">
+            <a v-if="liked">★</a>
+            <a v-if="!liked">☆</a>
+        </div>
+
+        <button v-if="isMine" @click="deleteTweet(thisTweet)">delete</button>
         <p>------------------------------</p>
     </div>
 </template>
 
 <script setup>
-const editable = ref(false)
-const liked = ref(false)
-const text = ref()
-const thisTweet = ref(props.tweet)
 // 親から受け取る値
 const props = defineProps({
     tweet: Object,
     authUser: Object
 })
+
+const editable = ref(false)
+const thisTweet = ref(props.tweet)
+const liked = ref(thisTweet.value.liked_by_me) // 誰にライクされたかを初期値に持ってこないといけない
+const text = ref(thisTweet.value.text)
+
+const isMine = props.tweet.user_id == props.authUser.id
+
 // 親が購読している値
 const emit = defineEmits()
 
@@ -53,6 +61,10 @@ const deleteTweet = (tweet) => {
 }
 
 const toggleLike = (tweet) => {
+    if (isMine) {
+        return
+    }
+
     $fetch(`http://localhost:3000/api/v1/tweets/${tweet.id}/likes`, {
         method: liked.value ? 'DELETE':'POST', // likeされているか否かで分岐
         body: {user_id: props.authUser.id}
@@ -73,15 +85,9 @@ const refreshTweet = (tweet) => {
     })
 }
 
-const isPublic = (tweet) => {
-    return tweet.status != "public"
-}
-
 const toggleEdit = () => {
+    if (isMine) {
     editable.value = !editable.value
+    }
 }
-
-onMounted(() => {
-    liked.value = props.tweet.user_id == props.authUser.id
-})
 </script>
--- pages/tweets.vue
+++ pages/tweets.vue
@@ -1,15 +1,34 @@
 <template>
+    <h1 v-if="!formVisible" @click="toggleFormVisible()">+</h1>
+
+    <form v-if="formVisible" @submit.prevent="createTweet()">
+        <input v-model="text" />
+        <button type="submit">submit</button>
+    </form>
+
     <div v-for="tweet in tweets" :key="tweet.id">
         <tweet-card :tweet="tweet" :authUser="authUser" @refreshAll="refresh"></tweet-card>
     </div>
 </template>
 
 <script setup>
-const { data: tweets, refresh } = await useFetch('http://localhost:3000/api/v1/tweets')
-// 仮でユーザー設定
+    // 仮でユーザー設定
 const authUser = {
     id: 2,
     name: "hoge",
     email: "fuga@gmail",
 }
+
+const formVisible = ref(false)
+
+const { data: tweets, refresh } = await useFetch(`http://localhost:3000/api/v1/tweets`)
+
+const toggleFormVisible = () => {
+    formVisible.value = !formVisible.value
+}
+
+const createTweet = () => {
+    
+}
+
 </script>
